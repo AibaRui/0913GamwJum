365f6896c150857d1b6d2d1558d39ad72cfae9e4
Aiba_0325 <bantan.ab325@gmail.com>
2022-09-13 12:28:49 +0900

    no message

--- Assets/Aiba/LoadScene.cs
+++ Assets/Aiba/LoadScene.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class LoadScene : MonoBehaviour
+public class LoadSceneAIba : MonoBehaviour
 {
     public void SceneLoad(string name)
     {
